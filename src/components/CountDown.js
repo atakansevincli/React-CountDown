8727cd6ea212393d196ab452789f43e74eaee108
Atakan Sevinçli <atakansevincli@Atakans-M1.local>
2023-06-08 17:16:46 +0300

    first commit

---
+++ src/components/CountDown.js
@@ -0,0 +1,32 @@
+import "./CountDown.css";
+
+const CountDown = () => {
+  var countDownDate = new Date("June 26, 2023 14:00:00").getTime();
+  let now, distance, days, hours, minutes, seconds;
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }, 1000);
+
+  return (
+    <p id="demo">
+      {days + "d " + hours + "h " + minutes + "m " + seconds + "s "}
+    </p>
+  );
+};
+
+export default CountDown;
